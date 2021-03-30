c95b35b0bcd677e9d66e673dcd35df3b42f0658c
laiito <maxim20demyanov@gmail.com>
2021-03-30 19:53:30 +0300

    Working with cookies

--- catalog/views.py
+++ catalog/views.py
@@ -9,15 +9,19 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
-
     num_instances_available = BookInstance.objects.filter(
         status__exact='a').count()
+    
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
+        'num_visits': num_visits,
     }
     return render(request, 'index.html', context=context)
 
